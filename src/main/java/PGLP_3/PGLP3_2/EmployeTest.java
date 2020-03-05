7450fa75da72b4b009fb16b24c19ec18db4201fb
KI Serge Laobien <sergelaobienk@gmail.com>
2020-03-05 21:41:22 +0000

    modif de la class Employetest

--- src/main/java/PGLP_3/PGLP3_2/EmployeTest.java
+++ src/main/java/PGLP_3/PGLP3_2/EmployeTest.java
@@ -1,5 +1,29 @@
 package PGLP_3.PGLP3_2;
 
+import java.util.ArrayList;
+
+import org.junit.Test;
+
 public class EmployeTest {
+	@Test
+	public void Testsalaire() {
+		Vendeur v =new Vendeur(10);
+		Vendeur v2= new Vendeur(20);
+		Employe e =new Employe(2010);
+		Employe e2 =new Employe(2005);
+		ArrayList<AllEmploye> listEmploye = new ArrayList <AllEmploye>();
+		listEmploye.add(e);
+		listEmploye.add(e2);
+		listEmploye.add(v);
+		listEmploye.add(v2);
+		double salaireTotal=0;
+		for(int i=0;i < listEmploye.size();i++) {
+			salaireTotal += listEmploye.get(i).salaire();
+		}
+		System.out.println("salaire total :"+salaireTotal);
+	}
+	 
+	 
+	
 
 }
